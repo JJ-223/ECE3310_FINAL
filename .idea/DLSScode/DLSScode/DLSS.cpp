c844667d25b6a38858b5fb0208a5f3aac6502474
JJ-223 <151877132+JJ-223@users.noreply.github.com>
2025-11-25 21:04:24 -0800

    Getting rid of unnecessary comments
    Understand code, write comments, make pptx

--- .idea/DLSScode/DLSScode/DLSS.cpp
+++ .idea/DLSScode/DLSScode/DLSS.cpp
@@ -311,26 +311,10 @@ void printSudoku(const vector<vector<int>>& grid, const string& label) {
 }
 
 int main() {
-    /*vector<vector<int>> puzzle = {
-        {5,3,0,0,7,0,0,0,0},
-        {6,0,0,1,9,5,0,0,0},
-        {0,9,8,0,0,0,0,6,0},
-        {8,0,0,0,6,0,0,0,3},
-        {4,0,0,8,0,3,0,0,1},
-        {7,0,0,0,2,0,0,0,6},
-        {0,6,0,0,0,0,2,8,0},
-        {0,0,0,4,1,9,0,0,5},
-        {0,0,0,0,8,0,0,7,9}
-    };*/
 
     vector<vector<int>> puzzle = generateRandomSudoku(40);
 
     printSudoku(puzzle, "Puzzle");
-    /* cout << "Puzzle:\n";
-    for (auto& row : puzzle) {
-        for (int v : row) cout << v << " ";
-        cout << "\n";
-    }*/
 
     DLX dlx(COLS);
     buildSudokuDLX(dlx);
@@ -340,12 +324,6 @@ int main() {
         auto out = extractSolution(dlx.solution);
         cout << "\n";
         printSudoku(out, "Solution");
-        /*cout << "\nSolution:\n";
-        for (auto& row : out) {
-            for (int v : row) cout << v << " ";
-            cout << "\n";
-        }*/
-        
     }
     else {
         cout << "No solution exists\n";
